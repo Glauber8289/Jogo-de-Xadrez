75f558965b6bd74e6baa9a2803da66a2acae870c
Glauber Peterson <Petersonglauber@hotmail.com>
2023-02-02 23:31:32 -0300

    Starting to implement Board and piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,14 @@
 package aplication;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Position pos = new Position(3, 5);
-       System.out.println(pos);
+     Board board = new Board(8, 8);
+     System.out.println(board);
 	}
 
 }
