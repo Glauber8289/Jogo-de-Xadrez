487309b2ad7f39900100f6d9eeb225d32de0f6b3
Glauber Peterson <Petersonglauber@hotmail.com>
2023-02-08 00:24:53 -0300

    ChessException and Chessposition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,9 +22,12 @@ public class ChessMatch {
 	   }
 	   return mat;
 	}
+	private void placeNewPiece(char colunm,int row,ChessPiece piece) {
+		board.placePiece(piece, new Chessposition(colunm, row).toPosition());
+	}
     public void initialSetup() {
-    	board.placePiece(new Rook(board, Color.White),new Position(2, 1));
-    	board.placePiece(new King(board, Color.Black),new Position(0, 4));
-    	board.placePiece(new King(board, Color.White),new Position(7, 4));
+    	placeNewPiece('b',6,new Rook(board, Color.White));
+    	placeNewPiece('e',8,new King(board, Color.Black));
+    	placeNewPiece('e',1,new King(board, Color.White));
     }
 }
---
+++ src/chess/Chessposition.java
@@ -0,0 +1,35 @@
+package chess;
+
+import boardgame.Position;
+
+public class Chessposition {
+   private char column;
+   private int row;
+
+   public Chessposition(char column, int row) {
+	if(column<'a'||column>'h'||row<1 || row>8) {               
+	 throw new ChessException("Erro ao instanciar chess position valores validos são de a1 até h8.");   
+	}
+	this.column = column;
+	this.row = row;
+}
+
+public char getColumn() {
+	return column;
+}
+
+public int getRow() {
+	return row;
+}
+
+ protected Position toPosition() {
+	 return new Position(8-row,column-'a');
+ }
+   protected static Chessposition fromPosition(Position position) {
+	   return new Chessposition((char)('a'-position.getColumn()),8-position.getRow());
+   }
+   @Override
+   public String toString() {
+	   return "" + column + row;
+   }
+}
