efe509865706c20f0b5054017aa28f38b5115c76
Glauber Peterson <Petersonglauber@hotmail.com>
2023-02-22 00:16:38 -0300

    Check Logic

--- src/chess/Chessposition.java
+++ src/chess/Chessposition.java
@@ -26,7 +26,7 @@ public int getRow() {
 	 return new Position(8 - row,column - 'a');
  }
    protected static ChessPosition fromPosition(Position position) {
-	   return new ChessPosition((char)('a'- position.getColumn()),8 - position.getRow());
+	   return new ChessPosition((char)('a'+position.getColumn()),8 - position.getRow());
    }
    @Override
    public String toString() {
