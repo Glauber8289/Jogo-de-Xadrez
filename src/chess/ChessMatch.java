095e327d26f7be61e1371c50111955b3d7864c6f
Glauber Peterson <Petersonglauber@hotmail.com>
2023-02-23 23:40:47 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -75,7 +75,8 @@ public class ChessMatch {
 			return (ChessPiece)capturedPiece;
 		}
 		private Piece makeMove(Position source, Position target){
-			Piece p = board.removePiece(source);
+			ChessPiece p = (ChessPiece)board.removePiece(source);
+			p.increaseMoveCount();
 			Piece capturedPiece = board.removePiece(target);
 			board.placePiece(p, target);
 			if (capturedPiece != null) {
@@ -85,7 +86,8 @@ public class ChessMatch {
 			return capturedPiece;
 		}
 		private void undoMove(Position source, Position target, Piece capturedPiece) {
-			Piece p = board.removePiece(target);
+			ChessPiece p = (ChessPiece)board.removePiece(target);
+			p.decreaseMoveCount();
 			board.placePiece(p, source);
 
 			if (capturedPiece != null) {
