7af51622b3822c111caa1debe95d5e21477ce5e0
Glauber8289 <petersonglauber@hotmail.com>
2023-04-08 23:58:34 -0300

    atualizando projeto

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -49,6 +49,7 @@ public class Program {
 				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
+				System.out.println(e.getMessage());
 				sc.nextLine();
 			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -108,7 +108,7 @@ public class UI {
 	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
-		System.out.println("Peças capturadas:");
+		System.out.println("Pecas capturadas:");
 		System.out.print("Brancas: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
